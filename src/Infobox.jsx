233739138d4cfc3634616476732dfe824664910d
czkoudy <czkoudy@gmail.com>
2020-11-25 10:22:41 +0000

    little cleanup

--- src/Infobox.jsx
+++ src/Infobox.jsx
@@ -12,7 +12,7 @@ function InfoBox({ title, cases, total, active, isRed, ...props }) {
         <h2 className={`infoBox__cases ${!isRed && "infoBox__cases--green"}`}>{cases}</h2>
 
         <Typography className="infoBox__total" color="textSecondary">
-          {total} Total
+          Total of {total}
         </Typography>
       </CardContent>
     </Card>
